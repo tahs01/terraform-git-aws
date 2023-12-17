91cef9cc6df2f9b74be8398198b7b95adab92057
Than Htike Soe <tahs01.ne@gmail.com>
2023-12-17 21:43:08 +0630

    modified manifacts by tahs

--- main.tf
+++ main.tf
@@ -1,18 +1,50 @@
 provider "aws" {
-  region = "us-east-2"  # Set your desired AWS region
+  region = var.aws_region
 }
 
-module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = "5.1.2"
-  cidr            = var.subnet_for_vpc
-  azs             = var.availiblity_zone
-  private_subnets = var.private_subnets_azs
-  public_subnets  = var.public_subnets_azs
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 locals {
-  cluster_name = "tahs-eks"
+  cluster_name = "education-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "education-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
 }
 
 module "eks" {
@@ -28,32 +60,20 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["m5.large"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
+      max_size     = 1  # Adjusted max_size to 1 for only one node
       desired_size = 1
     }
   }
 }
-
 
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
@@ -87,6 +107,9 @@ provider "kubectl" {
 }
 
 resource "kubectl_manifest" "deployment" {
-  yaml_body = file("todoapp-deployment.yaml")
-  depends_on = [module.eks]
+  yaml_body   = file("todoapp-deployment.yaml")
+  depends_on  = [module.eks]
+  provisioner "local-exec" {
+    command = "kubectl rollout restart deployment todoapp -n todoapp-ns"  # Replace <namespace> with your actual namespace
+  }
 }
--- variables.tf
+++ variables.tf
@@ -4,32 +4,32 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "subnet_for_vpc" {
-  description = "Subnet for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
+# variable "subnet_for_vpc" {
+#   description = "Subnet for VPC"
+#   type        = string
+#   default     = "10.0.0.0/16"
+# }
 
-variable "availiblity_zone" {
-  description = "Availibility Zones for VPC"
-  type        = list(string)
-  default     = ["us-east-2a", "us-east-2b"]
-}
+# variable "availiblity_zone" {
+#   description = "Availibility Zones for VPC"
+#   type        = list(string)
+#   default     = ["us-east-2a", "us-east-2b"]
+# }
 
-variable "private_subnets_azs" {
-  description = "Private Subnets for Availibility Zones"
-  type        = list(string)
-  default     = ["10.0.0.0/24", "10.0.1.0/24"]
-}
+# variable "private_subnets_azs" {
+#   description = "Private Subnets for Availibility Zones"
+#   type        = list(string)
+#   default     = ["10.0.0.0/24", "10.0.1.0/24"]
+# }
 
-variable "public_subnets_azs" {
-  description = "Public Subnets for Availibility Zones"
-  type        = list(string)
-  default     = ["10.0.2.0/24", "10.0.3.0/24"]
-}
+# variable "public_subnets_azs" {
+#   description = "Public Subnets for Availibility Zones"
+#   type        = list(string)
+#   default     = ["10.0.2.0/24", "10.0.3.0/24"]
+# }
 
-variable "eks_cluster" {
-  description = "name of cluster"
-  type        = string
-  default     = "tahs_eks_cluster"
+# variable "eks_cluster" {
+#   description = "name of cluster"
+#   type        = string
+#   default     = "tahs_eks_cluster"
 }
