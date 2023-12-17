735b2dcd5e2d1376dfd10b6fdf584bf3ed007151
Than Htike Soe <tahs01.ne@gmail.com>
2023-12-17 22:36:16 +0630

    added public ip auto assign in main.tf by tahs

--- main.tf
+++ main.tf
@@ -45,6 +45,9 @@ module "vpc" {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
+
+  # Ensure that public subnets have auto-assign public IP enabled
+  public_subnet_auto_assign_ip = true
 }
 
 module "eks" {
@@ -69,36 +72,9 @@ module "eks" {
       instance_types = ["m5.large"]
 
       min_size     = 1
-      max_size     = 1  # Adjusted max_size to 1 for only one node
+      max_size     = 1
       desired_size = 1
     }
-  }
-}
-
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
   }
 }
 
