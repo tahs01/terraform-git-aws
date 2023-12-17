cc8235f9a3babb4af304e9d691cb6d80138e1a5b
Than Htike Soe <tahs01.ne@gmail.com>
2023-12-17 15:11:50 +0630

    Updated main, versions, variables by tahs

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_eks_addon" "ebs-csi" {
   }
 }
 
-resource "kubectl_manifest" "deployment" {
-  yaml_body = file("todoap-deployment.yaml")
-  depends_on = [module.eks]
-}
+# resource "kubectl_manifest" "deployment" {
+#   yaml_body = file("todoap-deployment.yaml")
+#   depends_on = [module.eks]
+# }
